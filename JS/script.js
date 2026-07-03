2a0ad563a249109a1a26f8a4b015171781f91cd4
JOÃO GABRIEL PACÍFICO DA SILVA <joao_silva183@estudante.sesisenai.org.br>
2026-07-03 12:25:39 -0300

    joao

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
 function apertarBotao (){
     document.getElementById("botaocardMar").textContent = "Brasil 1 x 1 Marrocos";
 }
-function apertarBotao (){
+function apertarBotao2 (){
     document.getElementById("botaocardHai").textContent = "Brasil 3 x 0 Haiti"
 }
-function apertarBotao (){
+function apertarBotao3 (){
     document.getElementById("botaocardEsc").textContent = "Brasil 3 x 0 Escócia"
 }
